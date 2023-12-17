17420885db0582aa077e6dfe7c64a6c400480cbb
Alena Karenina <karena1q84@gmail.com>
2023-12-17 14:45:38 +0500

    refactor: removed unused

--- js/const.js
+++ js/const.js
@@ -5,16 +5,6 @@ const mainPoint = {
   lat: 35.68442,
   lng: 139.75425
 };
-const TITLES = ['Квартира', 'Бунгало', 'Отель'];
-const DESCRIPTIONS = ['Описание помещения 01', 'Описание помещения 02', 'Описание помещения 03'];
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-];
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const TIMES = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const offerTypeToTitle = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -40,4 +30,4 @@ const minPrice = {
   palace: 10000,
 };
 
-export { SIMILAR_OFFERS_COUNT, TITLES, DESCRIPTIONS, PHOTOS, TYPES, TIMES, FEATURES, offerTypeToTitle, Title, MAX_PRICE_PER_NIGHT, roomsCapacity, minPrice, MAP_START_ZOOM, mainPoint };
+export { SIMILAR_OFFERS_COUNT, offerTypeToTitle, Title, MAX_PRICE_PER_NIGHT, roomsCapacity, minPrice, MAP_START_ZOOM, mainPoint };

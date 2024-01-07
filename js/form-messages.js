6b4f17aa764dfc28e127bfa17df72f6c5c9475b9
Alena Karenina <karena1q84@gmail.com>
2024-01-07 22:59:00 +0500

    fix: changes naming to message at form

--- js/form-messages.js
+++ js/form-messages.js
@@ -10,33 +10,33 @@ const errorTemplate = document.querySelector('#error')
   .querySelector('.error');
 const errorElement = errorTemplate.cloneNode(true);
 
-const onMessageKeyDown = (evt) => {
+const onMessageKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     hideMessage();
   }
 };
 
-const closeMessageClick = () => {
+const onMessageClick = () => {
   hideMessage();
-  document.removeEventListener('click', closeMessageClick);
+  document.removeEventListener('click', onMessageClick);
 };
 
 function hideMessage () {
   document.querySelector('.modal').remove();
-  document.removeEventListener('keydown', onMessageKeyDown);
+  document.removeEventListener('keydown', onMessageKeydown);
 }
 
 const openSuccessMessage = () => {
   bodyElement.append(successElement);
-  successElement.addEventListener('click', closeMessageClick);
-  document.addEventListener('keydown', onMessageKeyDown);
+  successElement.addEventListener('click', onMessageClick);
+  document.addEventListener('keydown', onMessageKeydown);
 };
 
 const openErrorMessage = () => {
   bodyElement.append(errorElement);
-  errorElement.addEventListener('click', closeMessageClick);
-  document.addEventListener('keydown', onMessageKeyDown);
+  errorElement.addEventListener('click', onMessageClick);
+  document.addEventListener('keydown', onMessageKeydown);
 };
 
 export { openSuccessMessage, openErrorMessage };
